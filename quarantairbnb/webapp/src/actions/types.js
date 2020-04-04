37d7cccdb5ba2a396a5798e06c29e3469923f0bc
Marta Pacuszka <marta.pacuszka@cern.ch>
2020-04-04 13:53:28 +0200

    Add redux boilerplate

---
+++ quarantairbnb/webapp/src/actions/types.js
@@ -0,0 +1,8 @@
+// just examples for now
+
+import * as m from "./modifiers"
+
+export const HOSTING_REQUEST = {
+    root: "HOSTING_REQUEST",
+    modifiers: [m.GET_BY_ID, m.DELETE_BY_ID, m.PATCH_BY_ID]
+}
---
+++ quarantairbnb/webapp/src/components/LandingPage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Layout, Menu, Row,Col } from "antd";
+import StartingScreen from "./StartingScreen";
+import Header from "react-fullpage/dist/Header";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+
+function LandingPage() {
+  return (
+    <Layout className="layout">
+      <Header>
+        <Layout.Header>
+          <Row>
+            <Col span={12}>
+              <h1 style={{color: "#eee"}}>Quarantairbnb</h1>
+            </Col>
+            <Col span={12}>
+              <Menu
+                theme="dark"
+                mode="horizontal"
+                style={{ lineHeight: "64px", textAlign: "right" }}
+              >
+                <Menu.Item key="1"><Link to={'/register'}>Log in</Link></Menu.Item>
+                <Menu.Item key="2"><Link to={'/register'}>Sign up</Link></Menu.Item>
+              </Menu>
+            </Col>
+          </Row>
+        </Layout.Header>
+      </Header>
+      <Layout.Content>
+        <StartingScreen />
+      </Layout.Content>
+    </Layout>
+  );
+}
+
+export default withRouter(LandingPage);
