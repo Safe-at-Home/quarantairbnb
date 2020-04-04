279d279a80acbb067d919b17948dfde343ff5fa6
Maja Jabłońska <mjablonska@iterato.rs>
2020-04-04 15:32:25 +0200

    Added a separate page for login/registration

--- quarantairbnb/webapp/src/App.js
+++ quarantairbnb/webapp/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import LandingPage from "./components/LandingPage";
 import { Route, withRouter } from "react-router";
 import RegistrationPage from "./components/RegistrationPage";
+import LoginRegistrationScreen from "./LoginRegistrationScreen";
 
 // we should add routing here based on token/router I guess
 function App() {
   return (
     <>
       <Route exact path="/" component={LandingPage} />
-      <Route path="/register" component={RegistrationPage} />
+      <Route path="/register" component={LoginRegistrationScreen} />
     </>
   );
 }
