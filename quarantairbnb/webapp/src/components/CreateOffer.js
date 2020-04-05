97987020c9e51f3cd6bbe8c2f209d784d60ec6ec
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-04-05 11:41:45 +0200

    Can create offers (#22)

--- quarantairbnb/webapp/src/components/CreateOffer.js
+++ quarantairbnb/webapp/src/components/CreateOffer.js
@@ -1,19 +1,44 @@
-import React from "react";
-import { Alert, Input, Form, DatePicker, Button } from "antd";
+import React, { useState } from "react";
+import { Alert, Input, Form, DatePicker, Button, InputNumber } from "antd";
 import { postFromBody } from "../actions";
 import * as at from "../actions/types";
 import { connect } from "react-redux";
+import MapComp from "./MapComp";
+import { Segment, Header, Divider } from "semantic-ui-react";
+
+
 
 const CreateOffer = ({ postRequest, fetchOffers, setLoading }) => {
+  const [lat, setLat] = useState(52.231838);
+  const [long, setLong] = useState(21.0038063);
+  const updateParent = (mapLat, mapLong) => {
+    setLat(mapLat);
+    setLong(mapLong);
+  };
   const onFinish = async (values) => {
     console.log(values);
     // we should build a body here and submit
     setLoading(true);
-    const body = {};
+    const body = {
+      description: values.description,
+      start_date: values["range-picker"][0],
+      end_date: values["range-picker"][1],
+      latitude: lat,
+      longitude: long,
+      number_of_rooms: values.numberOfRooms,
+      area: values.area
+    };
     await postRequest(body);
     await fetchOffers();
     setLoading(false);
   };
+  const onChangeLat = (v) => {
+    setLat(v);
+  };
+  const onChangeLng = (v) => {
+    setLong(v);
+  };
+
   return (
     <>
       <Alert
@@ -25,6 +50,26 @@ const CreateOffer = ({ postRequest, fetchOffers, setLoading }) => {
         style={{ marginBottom: "20px" }}
       />
       <h1>Create a request</h1>
+      <Divider />
+      <MapComp lat={lat} long={long} updateParent={updateParent} />
+      <Divider />
+      <Segment basic>
+        <p>Latitude</p>
+        <InputNumber
+          min={0}
+          step={0.0000001}
+          value={lat}
+          onChange={onChangeLat}
+        />
+        <Divider />
+        <p>Longitude</p>
+        <InputNumber
+          min={0}
+          step={0.0000001}
+          value={long}
+          onChange={onChangeLng}
+        />
+      </Segment>
       <Form name="create-request" onFinish={onFinish}>
         <Form.Item
           name="range-picker"
@@ -46,6 +91,21 @@ const CreateOffer = ({ postRequest, fetchOffers, setLoading }) => {
         >
           <Input.TextArea />
         </Form.Item>
+        <Form.Item
+          name="numberOfRooms"
+          label="Number of rooms"
+          rules={[{ required: true }]}
+        >
+          <InputNumber min={0} />
+        </Form.Item>
+        <Form.Item
+          name="area"
+          label="Area"
+          rules={[{ required: true }]}
+        >
+          <InputNumber min={0} />
+        </Form.Item>
+
         <Button type="primary" htmlType="submit">
           Submit request
         </Button>
