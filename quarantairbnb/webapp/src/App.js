c7b6c6ebd12625fb3836f44d616b7d7b385c257e
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-08-29 14:18:25 +0300

    Add govtech logo in app (#28)

--- quarantairbnb/webapp/src/App.js
+++ quarantairbnb/webapp/src/App.js
@@ -1,26 +1,44 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
-import {Route, withRouter} from "react-router";
+import { Route, withRouter } from "react-router";
 import LoginRegistrationScreen from "./components/LoginRegistrationScreen";
 import GuestPanel from "./components/GuestPanel";
 import HostPanel from "./components/HostPanel";
 import StartHostPage from "./components/StartHostPage";
 import StartGuestPage from "./components/StartGuestPage";
 import AdminPanel from "./components/AdminPanel";
+import { Image, Grid } from "semantic-ui-react";
 
 // we should add routing here based on token/router I guess
 function App() {
   return (
     <>
-      <Route exact path="/" component={LandingPage}/>
-      <Route path="/login" render={(props) => <LoginRegistrationScreen {...props} isLogin={true}/>}/>
-      <Route path="/registerHost" render={(props) => <LoginRegistrationScreen {...props} isLogin={false} isHost={true}/>}/>
-      <Route path="/registerGuest" render={(props) => <LoginRegistrationScreen {...props} isLogin={false} isHost={false}/>}/>
-      <Route path="/guest" component={GuestPanel}/>
-      <Route path="/host" component={HostPanel}/> 
-      <Route path="/startHost" component={StartHostPage}/> 
-      <Route path="/startGuest" component={StartGuestPage}/> 
-      <Route path="/admin" component={AdminPanel}/> 
+      <Route exact path="/" component={LandingPage} />
+      <Route
+        path="/login"
+        render={(props) => (
+          <LoginRegistrationScreen {...props} isLogin={true} />
+        )}
+      />
+      <Route
+        path="/registerHost"
+        render={(props) => (
+          <LoginRegistrationScreen {...props} isLogin={false} isHost={true} />
+        )}
+      />
+      <Route
+        path="/registerGuest"
+        render={(props) => (
+          <LoginRegistrationScreen {...props} isLogin={false} isHost={false} />
+        )}
+      />
+      <Route path="/guest" component={GuestPanel} />
+      <Route path="/host" component={HostPanel} />
+      <Route path="/startHost" component={StartHostPage} />
+      <Route path="/startGuest" component={StartGuestPage} />
+      <Route path="/admin" component={AdminPanel} />
+      <br />
+      <Image src="logo-govtech.png" size="small" />
     </>
   );
 }
